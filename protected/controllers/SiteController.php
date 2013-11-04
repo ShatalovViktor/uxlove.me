a1f0715992b64c1d14ba94dbfd6d525deed9484a
Shatalov Viktor <shatalov.viktor@gmail.com>
2013-11-04 21:17:15 +0200

    ВЫвод информации о пользователе
    В sitecontroller создал экземпляр класс USER (модель)
    потом с помощью функции Yii::app()->user->getId узнал id текущего
    пользователя, потом с помощью метода findByPk по id пользователя нашел
    всю его информацию и вывел на главной странице сайта

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,7 +29,10 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+           $model = new User;
+            $idUser=Yii::app()->user->getId();
+            $resUser = $model::model()->findByPk($idUser);
+		$this->render('index',array('user'=> $resUser));
 	}
 
 	/**
